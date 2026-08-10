79cc34be3d1f99cf30d061648cfb40a15186ada9
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-10 20:56:16 +0530

    initialised with spring boot

---
+++ src/test/java/com/sumeet/joblisting/JoblistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumeet.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
